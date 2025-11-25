faf673572026485c94ca72e881bad0542aa2caac
ivan <cvcv1843@gmail.com>
2025-11-25 10:34:16 +0400

    Create Exercise07.py
    Counting numbers

---
+++ Exercise 07 - Some Counting/Exercise07.py
@@ -0,0 +1,17 @@
+print("a loop that counts up from 0 to 50 in increments of 1")
+for i in range(51):
+    print(i)
+print("a loop that counts down from 50 to 0 in decrements of 1")
+for i in range(50,-1,-1):
+    print(i)
+print("a loop that counts up from 30 to 50 in increments of 1")
+for i in range (30,51):
+    print(i)
+print("a loop that counts down from 50 to 10 in decrements of 2")
+for i in range (50,8,-2):
+    print(i)
+print("a loop that counts up from 100 to 200 in increments of 5")
+for i in range(100,205,5):
+    print(i)
+
+
