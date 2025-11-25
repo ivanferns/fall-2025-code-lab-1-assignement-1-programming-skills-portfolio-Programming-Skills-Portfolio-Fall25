25cd06635ad614125aee509a94767169c50363d2
ivan <cvcv1843@gmail.com>
2025-11-25 10:33:20 +0400

    Create Exercise04.py
    Primitive quiz

---
+++ Exercise 04 -Primitive Quiz/Exercise04.py
@@ -0,0 +1,18 @@
+capital={"France":"paris"
+          ,"India":"delhi"
+          ,"Pakistan":"Islamabad"}
+a=input("Enter the capital of France:")
+if a==capital["paris"]:
+    print("The answer is correct")
+else:
+    print("The answer is wrong")
+a=input("Enther the capital of India:")
+if a==capital["delhi"]:
+    print("The answer is right")
+else:
+    print("The answer is wrong")
+a=input("Enter the capital of Pakistan")
+if a==capital["Islamabad"]:
+    print("The answer is right")
+else:
+    print("The answer is wrong")
